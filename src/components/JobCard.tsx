8a42612c2393cfb5f2353c943d9ef58ae8e5f97f
PedroAlmeidaFetchly <107511028+PedroAlmeidaFetchly@users.noreply.github.com>
2022-08-19 19:56:59 -0300

    module css working

--- src/components/JobCard.tsx
+++
@@ -1,5 +0,0 @@
-const JobCard = () => {
-  return <h2>Job Card</h2>;
-};
-
-export default JobCard;
---
+++ src/components/JobCard/index.tsx
@@ -0,0 +1,7 @@
+import styles from "./JobCard.module.css";
+
+const JobCard = () => {
+  return <h2 className={styles.error}>Job Card</h2>;
+};
+
+export default JobCard;

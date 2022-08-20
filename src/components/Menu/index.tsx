78a6ca83aa6cacaf25cbf51aaa42c5e230607008
PedroAlmeidaFetchly <107511028+PedroAlmeidaFetchly@users.noreply.github.com>
2022-08-20 14:12:05 -0300

    show hide menu item logic

--- src/components/Menu/index.tsx
+++ src/components/Menu/index.tsx
@@ -1,21 +1,30 @@
+import { useState } from "react";
 import Image from "next/image";
 import styles from "./Menu.module.css";
 
 const Menu = () => {
+  const [showMenu, setShowMenu] = useState(false);
   return (
     <div id={styles.root}>
       <div>👶💻</div>
-      <div>
-        <Image src={"/icons/menu-burger.svg"} width={18} height={18} />
-        <nav>
-          <ul id={styles.menuContainer}>
-            <li className={styles.menuItem}>Home 🏠</li>
-            <li className={styles.menuItem}>Busca 🔍</li>
-            <li className={styles.menuItem}>Parceiros 🤝</li>
-            <li className={styles.menuItem}>Contato 📞</li>
-            <li className={styles.menuItem}>Login</li>
-          </ul>
-        </nav>
+      <div id={styles.navContainer}>
+        <Image
+          src={"/icons/menu-burger.svg"}
+          width={18}
+          height={18}
+          onClick={() => setShowMenu(!showMenu)}
+        />
+        {showMenu ? (
+          <nav onMouseLeave={() => setShowMenu(false)}>
+            <ul id={styles.menuContainer}>
+              <li className={styles.menuItem}>Home 🏠</li>
+              <li className={styles.menuItem}>Busca 🔍</li>
+              <li className={styles.menuItem}>Parceiros 🤝</li>
+              <li className={styles.menuItem}>Contato 📞</li>
+              <li className={styles.menuItem}>Login</li>
+            </ul>
+          </nav>
+        ) : null}
       </div>
     </div>
   );

5c3da1e8b81d68e51779813b0c87a8831c82bb4a
PedroAlmeidaFetchly <107511028+PedroAlmeidaFetchly@users.noreply.github.com>
2022-08-19 22:07:52 -0300

    add style before in stack to the new component

--- src/components/StackBadge/index.tsx
+++ src/components/StackBadge/index.tsx
@@ -1,6 +1,10 @@
+import styles from "./StackBadge.module.css";
+
 interface StackBadgeProps {
   name: string;
 }
 
-const StackBadge = ({ name }: StackBadgeProps) => <h6>{name}</h6>;
+const StackBadge = ({ name }: StackBadgeProps) => (
+  <h6 id={styles.root}>{name}</h6>
+);
 export default StackBadge;

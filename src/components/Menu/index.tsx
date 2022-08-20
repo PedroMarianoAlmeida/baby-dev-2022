11c3bb85fc525617546c0e618707eb79c607605a
PedroAlmeidaFetchly <107511028+PedroAlmeidaFetchly@users.noreply.github.com>
2022-08-20 12:28:55 -0300

    add initial html structure

--- src/components/Menu/index.tsx
+++ src/components/Menu/index.tsx
@@ -1,5 +1,21 @@
+import Image from "next/image";
+
 const Menu = () => {
-  return <h2>Menu</h2>;
+  return (
+    <div>
+      <div>👶💻</div>
+      <Image src={"/icons/menu-burger.svg"} width={18} height={18} />
+      <nav>
+        <ul>
+          <li>Home 🏠</li>
+          <li>Busca 🔍</li>
+          <li>Parceiros 🤝</li>
+          <li>Contato 📞</li>
+          <li>Login</li>
+        </ul>
+      </nav>
+    </div>
+  );
 };
 
 export default Menu;

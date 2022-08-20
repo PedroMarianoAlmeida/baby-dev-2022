432a278405dc7acec7ec9e8ef3b23dde1ee6b82f
PedroAlmeidaFetchly <107511028+PedroAlmeidaFetchly@users.noreply.github.com>
2022-08-20 19:13:26 -0300

    add html login

--- src/components/Menu/Login.tsx
+++ src/components/Menu/Login.tsx
@@ -1,14 +1,32 @@
+import Image from "next/image";
 import styles from "./Menu.module.css";
 
 const Login = () => {
-  return <LoginUI isLogged={false} />;
+  return (
+    <LoginUI
+      isLogged={true}
+      userData={{
+        name: "Sarah",
+        image: "/sample/login-avatar.png",
+      }}
+    />
+  );
 };
 
-const LoginUI = ({ isLogged }) => {
+const LoginUI = ({ isLogged, userData }) => {
+  const { name, image } = userData;
   const { menuItem, notLogged } = styles;
   return (
     <li className={menuItem}>
-      {isLogged ? <></> : <div id={notLogged}>Login</div>}
+      {isLogged ? (
+        <div>
+          <div>Oi, {name}</div>
+          <p>Log out</p>
+          <Image src={image} width={22} height={22} />
+        </div>
+      ) : (
+        <div id={notLogged}>Login</div>
+      )}
     </li>
   );
 };

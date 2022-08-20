4d7bcfcbdb49d3b49af20483329778ba1b19296e
PedroAlmeidaFetchly <107511028+PedroAlmeidaFetchly@users.noreply.github.com>
2022-08-20 18:56:14 -0300

    not logged component

--- src/components/Menu/Login.tsx
+++ src/components/Menu/Login.tsx
@@ -1,9 +1,16 @@
+import styles from "./Menu.module.css";
+
 const Login = () => {
-  return <LoginUI />;
+  return <LoginUI isLogged={false} />;
 };
 
-const LoginUI = () => {
-  return <h2>Login</h2>;
+const LoginUI = ({ isLogged }) => {
+  const { menuItem, notLogged } = styles;
+  return (
+    <li className={menuItem}>
+      {isLogged ? <></> : <div id={notLogged}>Login</div>}
+    </li>
+  );
 };
 
 export default Login;

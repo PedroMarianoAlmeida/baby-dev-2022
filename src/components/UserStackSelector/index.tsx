c46a936b02631a422585811bf370a6889dbe3fe1
PedroAlmeidaFetchly <107511028+PedroAlmeidaFetchly@users.noreply.github.com>
2022-08-20 23:49:21 -0300

    ass searchContainer style

--- src/components/UserStackSelector/index.tsx
+++ src/components/UserStackSelector/index.tsx
@@ -1,19 +1,16 @@
 import Image from "next/image";
+import styles from "./UserStackSelector.module.css";
 
 const UserStackSelector = () => {
   return <UserStackSelectorUI />;
 };
 
 const UserStackSelectorUI = () => {
+  const { searchContainer } = styles;
   return (
     <div>
-      <div>
-        <Image
-          // id={styles.logo}
-          src={"/icons/magnifying-glass.svg"}
-          width={22}
-          height={22}
-        />
+      <div id={searchContainer}>
+        <Image src={"/icons/magnifying-glass.svg"} width={22} height={22} />
       </div>
       <h2>UserStackSelector</h2>
     </div>

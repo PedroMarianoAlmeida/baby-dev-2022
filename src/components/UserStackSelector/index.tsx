09614de580c888811b7121bc876e4f6a6c867fa9
PedroAlmeidaFetchly <107511028+PedroAlmeidaFetchly@users.noreply.github.com>
2022-08-21 18:07:21 -0300

    implement useDebouce in UserStackSelector

--- src/components/UserStackSelector/index.tsx
+++ src/components/UserStackSelector/index.tsx
@@ -1,6 +1,7 @@
-import { useState } from "react";
+import { useEffect, useState } from "react";
 import Image from "next/image";
 import styles from "./UserStackSelector.module.css";
+import useDebounce from "src/hooks/useDebounce";
 
 const UserStackSelector = () => {
   return <UserStackSelectorUI />;
@@ -8,14 +9,30 @@ const UserStackSelector = () => {
 
 const UserStackSelectorUI = () => {
   const [showOptions, setShowOptions] = useState(false);
+  const [isMouseLeavesMenu, setIsMouseLeavesMenu] = useState(false);
+  const debouncedIsMouseLeavesMenu = useDebounce(isMouseLeavesMenu, 350);
+
   const { root, searchContainer, stackContainer } = styles;
+
+  useEffect(() => {
+    if (debouncedIsMouseLeavesMenu) {
+      setShowOptions(false);
+      setIsMouseLeavesMenu(false);
+    }
+  }, [debouncedIsMouseLeavesMenu]);
 
   return (
     <div id={root}>
       <div id={searchContainer} onClick={() => setShowOptions(!showOptions)}>
         <Image src={"/icons/magnifying-glass.svg"} width={22} height={22} />
       </div>
-      {showOptions ? <div id={stackContainer}></div> : null}
+      {showOptions ? (
+        <div
+          id={stackContainer}
+          onMouseLeave={() => setIsMouseLeavesMenu(true)}
+          onMouseEnter={() => setIsMouseLeavesMenu(false)}
+        ></div>
+      ) : null}
     </div>
   );
 };

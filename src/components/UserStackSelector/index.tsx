60fe4d7998e10124c7e8f96a10025dfcb8c1a361
PedroAlmeidaFetchly <107511028+PedroAlmeidaFetchly@users.noreply.github.com>
2022-08-20 23:44:41 -0300

    inserting magnifying-glass image

--- src/components/UserStackSelector/index.tsx
+++ src/components/UserStackSelector/index.tsx
@@ -1,9 +1,23 @@
+import Image from "next/image";
+
 const UserStackSelector = () => {
   return <UserStackSelectorUI />;
 };
 
 const UserStackSelectorUI = () => {
-  return <h2>UserStackSelector</h2>;
+  return (
+    <div>
+      <div>
+        <Image
+          // id={styles.logo}
+          src={"/icons/magnifying-glass.svg"}
+          width={22}
+          height={22}
+        />
+      </div>
+      <h2>UserStackSelector</h2>
+    </div>
+  );
 };
 
 export default UserStackSelector;

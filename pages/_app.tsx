374a8edccf2f62632f19a0972564db5f6f28b64f
PedroAlmeidaFetchly <107511028+PedroAlmeidaFetchly@users.noreply.github.com>
2022-09-11 17:38:47 -0300

    add container to nothing be hidden under menu

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -4,10 +4,10 @@ import Menu from "../src/components/Menu";
 
 function MyApp({ Component, pageProps }: AppProps) {
   return (
-    <>
+    <div id="myapp-container">
       <Menu />
       <Component {...pageProps} />
-    </>
+    </div>
   );
 }
 
